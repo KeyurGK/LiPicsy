1229df3330608e266699f3f2e845edbcbcec717f
KeyurGK <56074740+KeyurGK@users.noreply.github.com>
2023-07-25 18:51:05 +0530

    pages & comp dor prompt & result

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { preview } from "../assets";
+import { getRandomPrompt } from "../utils";
+import { FormField, Loader } from "../components";
 
 const CreatePost = () => {
   return <div>CreatePost</div>;
