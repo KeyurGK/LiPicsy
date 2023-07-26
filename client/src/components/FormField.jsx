66146c56d9f2ffb830dbf08108200e8558d2de4b
KeyurGK <56074740+KeyurGK@users.noreply.github.com>
2023-07-26 18:56:45 +0530

    structured formField

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,7 +1,39 @@
 import React from "react";
 
-const FormField = () => {
-  return <div>FormField</div>;
+const FormField = (props) => {
+  const { form, preview } = props;
+  return (
+    <div>
+      <div className="flex items-center gap-2 mb-2">
+        <label
+          htmlFor={props.name}
+          className="block text-sm font-medium text-gray-900"
+        >
+          {props.labelName}
+        </label>
+        {props.isSurpriseMe && (
+          <button
+            type="button"
+            onClick={props.handleSurpriseMe}
+            className="font semibold text-xs bg-[#ECEF1] py-1 px-2 rounded-[5px] text-black"
+          >
+            Surprise Me
+          </button>
+        )}
+      </div>
+      <input
+        type={props.type}
+        id={props.name}
+        name={props.name}
+        placeholder={props.placeholder}
+        value={props.value}
+        onChange={props.handleChange}
+        required
+        className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff]
+        outline-none block w-full p-3"
+      />
+    </div>
+  );
 };
 
 export default FormField;
